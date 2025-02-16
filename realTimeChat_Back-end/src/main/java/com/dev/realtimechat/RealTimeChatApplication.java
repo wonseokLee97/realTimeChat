34497adc70bdfd1bb2d891b0ddbe436e659ffff3
wonseok970 <bulnabang99@gmail.com>
2025-02-17 02:06:19 +0900

    feat: add @EnableMongoAuditing

--- realTimeChat_Back-end/src/main/java/com/dev/realtimechat/RealTimeChatApplication.java
+++ realTimeChat_Back-end/src/main/java/com/dev/realtimechat/RealTimeChatApplication.java
@@ -2,10 +2,14 @@ package com.dev.realtimechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMongoAuditing
+@ComponentScan(basePackages = "com.dev")
 public class RealTimeChatApplication {
 
 	public static void main(String[] args) {
