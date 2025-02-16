2fb7b2115c4b82507f4935425821c9dff2218d23
wonseok970 <bulnabang99@gmail.com>
2025-02-17 02:08:41 +0900

    feat: add paging method

--- realTimeChat_Back-end/src/main/java/com/dev/realtimechat/chat/application/ChatService.java
+++ realTimeChat_Back-end/src/main/java/com/dev/realtimechat/chat/application/ChatService.java
@@ -1,7 +1,7 @@
 package com.dev.realtimechat.chat.application;
 
-import com.dev.realtimechat.shared.common.dto.ChatMessageDto;
 import com.dev.realtimechat.chat.domain.Chat;
+import com.dev.realtimechat.shared.global.dto.ChatMessageDto;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,5 +9,6 @@ import java.util.List;
 @Component
 public interface ChatService {
     Chat createChat(ChatMessageDto.ChatMessageRequest request, String randId, String nameTag, String ipAddress);
-    List<ChatMessageDto.ChatMessageResponse> getChatList(String chatRoomId, int limit, int offset );
+    List<ChatMessageDto.ChatMessageResponse> getChatListByOffset(String chatRoomId, int limit, int offset );
+    List<ChatMessageDto.ChatMessageResponse> getChatListByLastMessageId(String chatRoomId, int limit, int lastMessageId);
 }
